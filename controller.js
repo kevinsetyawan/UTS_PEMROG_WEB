02c039d046627bc9e6bdfab9fa0f2dadec9e80b0
kevin <kevinsetyawan12360@outlook.com>
2020-05-01 14:04:22 +0700

    berhasil tampil data tabel montir berdasarkan id

--- controller.js
+++ controller.js
@@ -4,5 +4,31 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res){
-    response.ok("Aplikasi REST API ku berjalan yey!",res)
+    response.ok("UTS pemrograman website ku berjalan yey!",res)
+};
+
+//menampilkan semua data tabel sparepart berdasarkan id
+exports.tampilsemuaberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_sparepart WHERE id_sparepart = ?', [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows,res)
+        }
+    });
+};
+
+//menampilkan semua data tabel montir berdasarkan id
+exports.tampilsemuaberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_montir WHERE id_montir = ?', [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows,res)
+        }
+    });
 };
