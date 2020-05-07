534131a62bf33e1c41f487c7f537db3660fa5b1d
kevin <kevinsetyawan12360@outlook.com>
2020-05-07 09:46:47 +0700

    menghapus data sparepart

--- controller.js
+++ controller.js
@@ -282,3 +282,17 @@ exports.hapusMontir = function(req, res){
         }
     });
 };
+
+//Menghapus Data Sparepart
+exports.hapusSparepart = function(req, res){
+    var id = req.body.id_sparepart;
+    connection.query('DELETE FROM t_sparepart WHERE id_sparepart=?', [id],
+    function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok("Berhasil Hapus Data", res)
+        }
+    });
+};
+
