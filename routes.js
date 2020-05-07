28a12c13d8c1eca661c05fff329f9416bb154c4b
kevin <kevinsetyawan12360@outlook.com>
2020-05-07 09:03:32 +0700

    tambah data sparepart

--- routes.js
+++ routes.js
@@ -24,4 +24,7 @@ module.exports = function (app) {
     app.route('/tambahmontir')
         .post(jsonku.tambahmontir);
 
+    app.route('/tambahsparepart')
+        .post(jsonku.tambahsparepart);
+
 } 
